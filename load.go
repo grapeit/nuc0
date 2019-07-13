5ab2472378502312bdafa868315deae605703ae2
grapeit <accel@the-grape.com>
2019-07-12 22:21:03 -0500

    adjusted colors, blinking threshold

--- load.go
+++ load.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/pkg/errors"
 	"io/ioutil"
+	"runtime"
 	"strconv"
 	"strings"
 	"time"
@@ -16,30 +17,47 @@ const (
 )
 
 type colorByLoad struct {
-	load  float64
+	load float64
 	color string
+	brightness int
 }
 
-var midColorsByLoad = []colorByLoad {
-	{0.0, "white"},
-	{0.5, "blue"},
-	{1.0, "cyan"},
-	{2.0, "green"},
-	{3.0, "yellow"},
-	{4.0, "pink"},
+var blinkLoadThreshold = 4.0
+var blinkLedMode = "none"
+
+var colorsByLoad = []colorByLoad {
+	{0, "white", 80},
+	{0.02, "blue", 8},
+	{0.05, "blue", 20},
+	{0.10, "blue", 40},
+	{0.15, "blue", 80},
+	{0.25, "cyan", 50},
+	{0.5, "green", 80},
+	{0.75, "yellow", 60},
+	{1.0, "pink", 60},
+	{0, "red", 80},
 }
 
-func loadToColor(load float64) string {
-	for _, lc := range midColorsByLoad {
+func init() {
+	cores := float64(runtime.NumCPU())
+	blinkLoadThreshold *= cores
+	for i, _ := range colorsByLoad[:len(colorsByLoad) - 1] {
+		colorsByLoad[i].load *= cores
+	}
+}
+
+func loadToColor(load float64) (string, int) {
+	for _, lc := range colorsByLoad[:len(colorsByLoad) - 1] {
 		if load < lc.load {
-			return lc.color
+			return lc.color, lc.brightness
 		}
 	}
-	return "red"
+	last := colorsByLoad[len(colorsByLoad) - 1]
+	return last.color, last.brightness
 }
 
-func setLedColor(color string) {
-	cmd := fmt.Sprintf("ring,80,none,%s", color)
+func setRingLed(color string, brightness int) {
+	cmd := fmt.Sprintf("ring,%d,%s,%s", brightness, blinkLedMode, color)
 	err := ioutil.WriteFile(ledDriverFile, []byte(cmd), 0644)
 	if err != nil {
 		fmt.Println("set error:", err)
@@ -59,6 +77,11 @@ func getLoadAverage() (float64, error) {
 	if err != nil {
 		return -1, err
 	}
+	if lv > blinkLoadThreshold {
+		blinkLedMode = "fade_fast"
+	} else {
+		blinkLedMode = "none"
+	}
 	return lv, nil
 }
 
@@ -66,9 +89,9 @@ func loadAverageMonitor() {
 	for {
 		load, err := getLoadAverage()
 		if err != nil {
-			fmt.Println("monitor error:", err)
+			fmt.Println("getLoadAverage error", err)
 		}
-		setLedColor(loadToColor(load))
+		setRingLed(loadToColor(load))
 		time.Sleep(loadFeedInterval)
 	}
 }
