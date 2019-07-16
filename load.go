ad069b016a2a7e30f5c88dea7e738e90d8c98f6d
grapeit <accel@the-grape.com>
2019-07-15 22:31:02 -0500

    update color on change only

--- load.go
+++ load.go
@@ -16,48 +16,46 @@ const (
 	loadFeedInterval = 5 * time.Second
 )
 
-type colorByLoad struct {
+type ledColorConfig struct {
 	load float64
 	color string
 	brightness int
+	blink string
 }
 
-var colorsByLoad = []colorByLoad {
-	{0, "white", 80},
-	{0.02, "blue", 10},
-	{0.05, "blue", 20},
-	{0.10, "blue", 40},
-	{0.15, "blue", 80},
-	{0.25, "cyan", 50},
-	{0.5, "green", 80},
-	{0.75, "yellow", 60},
-	{1.0, "pink", 60},
-	{0, "red", 80},
+var colorsByLoad = []ledColorConfig {
+	{0, "white", 80, "none"},
+	{0.02, "blue", 10, "none"},
+	{0.05, "blue", 20, "none"},
+	{0.10, "blue", 40, "none"},
+	{0.15, "blue", 80, "none"},
+	{0.25, "cyan", 50, "none"},
+	{0.5, "green", 80, "none"},
+	{0.75, "yellow", 60, "none"},
+	{1.0, "pink", 60, "none"},
+	{2.0, "red", 80, "none"},
+	{4.0, "red", 100, "none"},
+	{0, "red", 100, "fade_fast"},
 }
-
-var blinkLoadThreshold = 4.0
-var blinkLedMode = "none"
 
 func init() {
 	cores := float64(runtime.NumCPU())
-	blinkLoadThreshold *= cores
-	for i, _ := range colorsByLoad[:len(colorsByLoad) - 1] {
+	for i, _ := range colorsByLoad {
 		colorsByLoad[i].load *= cores
 	}
 }
 
-func loadToColor(load float64) (string, int) {
-	for _, lc := range colorsByLoad[:len(colorsByLoad) - 1] {
-		if load < lc.load {
-			return lc.color, lc.brightness
+func getColorByLoad(load float64) ledColorConfig {
+	for i := 0; i < len(colorsByLoad); i++ {
+		if load < colorsByLoad[i].load {
+			return colorsByLoad[i]
 		}
 	}
-	last := colorsByLoad[len(colorsByLoad) - 1]
-	return last.color, last.brightness
+	return colorsByLoad[len(colorsByLoad) - 1]
 }
 
-func setRingLed(color string, brightness int) {
-	cmd := fmt.Sprintf("ring,%d,%s,%s", brightness, blinkLedMode, color)
+func setRingColor(color ledColorConfig) {
+	cmd := fmt.Sprintf("ring,%d,%s,%s", color.brightness, color.blink, color.color)
 	err := ioutil.WriteFile(ledDriverFile, []byte(cmd), 0644)
 	if err != nil {
 		fmt.Println("set error:", err)
@@ -77,21 +75,21 @@ func getLoadAverage() (float64, error) {
 	if err != nil {
 		return -1, err
 	}
-	if lv > blinkLoadThreshold {
-		blinkLedMode = "fade_fast"
-	} else {
-		blinkLedMode = "none"
-	}
 	return lv, nil
 }
 
 func loadAverageMonitor() {
+	var prevColor ledColorConfig
 	for {
 		load, err := getLoadAverage()
 		if err != nil {
 			fmt.Println("getLoadAverage error", err)
 		}
-		setRingLed(loadToColor(load))
+		var color = getColorByLoad(load)
+		if color != prevColor {
+			setRingColor(color)
+			prevColor = color
+		}
 		time.Sleep(loadFeedInterval)
 	}
 }
